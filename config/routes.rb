9404ba83de4bb98b75a44bc3897be94c00d072d6
kirka <kirka121@gmail.com>
2016-02-11 11:35:49 -0500

    moved destroy to api controller because it returns json instead of rendering a server side javascript file

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root 'landing#index'
 
   namespace :api, defaults: { format: :json } do
-    resources :events, only: [:index]
+    resources :events, only: [:index, :destroy]
   end
 
-  resources :events, except: [:index, :edit]
+  resources :events, except: [:index, :edit, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
